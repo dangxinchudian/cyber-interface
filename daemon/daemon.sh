985b48b8c918c3ed86767335bc09cee288a259a1
Twwy <zje2008@qq.com>
2013-06-05 16:22:17 +0800

    a

--- daemon/daemon.sh
+++ daemon/daemon.sh
@@ -2,9 +2,12 @@
 
 daemon1="constant.php"
 daemon2="server.php"
+daemon3="attack.php"
 
 kill -9 `ps axu | grep "$daemon1" | awk '{print $2}'` > /dev/null 2>&1
 kill -9 `ps axu | grep "$daemon2" | awk '{print $2}'` > /dev/null 2>&1
+kill -9 `ps axu | grep "$daemon3" | awk '{print $2}'` > /dev/null 2>&1
 
 nohup php ./$daemon1 &
 nohup php ./$daemon2 &
+nohup php ./$daemon3 &
