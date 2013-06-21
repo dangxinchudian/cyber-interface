4093c8244e9f1374fcf91614344afdcdea08485c
Twwy <zje2008@qq.com>
2013-06-21 13:33:54 +0800

    a

--- controller/other/auth-list.php
+++ controller/other/auth-list.php
@@ -2,12 +2,12 @@
 
 	$user = model('user');
 	$user_id = $user->sessionCheck(function(){
-		if(isset($_POST) && $_POST['token'] == 'cf05dcc346658899469f2d50311a09e4'){
+		if(isset($_POST['token']) && $_POST['token'] == 'cf05dcc346658899469f2d50311a09e4'){
 
 		}else json(false, '未登录');
 	});
 	$admin = $user->adminCheck();
-	if(isset($_POST) && $_POST['token'] == 'cf05dcc346658899469f2d50311a09e4') $admin = true;
+	if(isset($_POST['token']) && $_POST['token'] == 'cf05dcc346658899469f2d50311a09e4') $admin = true;
 	if(!$admin) json(false, '非管理员无权访问！');
 
 	$model = new model;
